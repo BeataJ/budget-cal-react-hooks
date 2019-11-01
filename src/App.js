c84dee28a7fb0ad2dfe36e19fc82c3d10f229a02
beata <beata@emc2wave.com>
2019-11-01 16:35:27 -0700

    add parseInt

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
         <span className="total">
           $
           {expenses.reduce((acc, curr) => {
-            return (acc += curr.amount);
+            return (acc += parseInt(curr.amount));
           }, 0)}
         </span>
       </h1>
