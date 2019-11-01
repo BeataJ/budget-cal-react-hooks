9c18c834a2f6c7255c164b65cd7ec2cdb0e400fd
beata <beata@emc2wave.com>
2019-11-01 13:35:06 -0700

    add const initialExpense array

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Alert from './components/Alert';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 
+const initialExpenses = [
+  {id:1, charge: 'rent', amount: 1600},
+  {id:2, charge: 'car payment', amount: 400},
+  {id:3, charge: 'credit card bill', amount: 1200}
+]
+
 function App() {
   return (
     <>
