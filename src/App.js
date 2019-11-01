ee7f1c1a6969d85305c38bd1fcc7b2226a807cb4
beata <beata@emc2wave.com>
2019-11-01 13:40:38 -0700

    import uuid and invote them

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Alert from './components/Alert';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
+import uuid from 'uuid/v4';
 
 const initialExpenses = [
-  {id:1, charge: 'rent', amount: 1600},
-  {id:2, charge: 'car payment', amount: 400},
-  {id:3, charge: 'credit card bill', amount: 1200}
+  {id:uuid(), charge: 'rent', amount: 1600},
+  {id:uuid(), charge: 'car payment', amount: 400},
+  {id:uuid(), charge: 'credit card bill', amount: 1200}
 ]
+
+console.log(initialExpenses);
 
 function App() {
   return (
