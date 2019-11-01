3e8555258f97941ad0971009098f0131722da604
beata <beata@emc2wave.com>
2019-11-01 15:41:39 -0700

    create form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
 import { MdSend } from "react-icons/md";
 
 const ExpenseForm = () => {
   return (
-    <div>
-      Hi from expense form
-    </div>
-  )
-}
+    <form>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="e.g. rent"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="text"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="e.g. 100"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default ExpenseForm
+export default ExpenseForm;
