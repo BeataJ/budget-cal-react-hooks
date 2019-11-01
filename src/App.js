86cf76f465fa0cbdc060adcb471f553add824fbb
beata <beata@emc2wave.com>
2019-11-01 16:01:37 -0700

    add single state and functionality

--- src/App.js
+++ src/App.js
@@ -14,8 +14,30 @@ const initialExpenses = [
 // console.log(initialExpenses);
 
 function App() {
+  // ******** state values *************
+  // all expenses, add expense
   const [expenses, setExpenses] = useState(initialExpenses);
-  // console.log(expenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  //  ********** functionality *********
+  const handleCharge = e => {
+    console.log(`charge: ${e.target.value}`);
+
+    setCharge(e.target.value)
+  }
+
+  const handleAmount = e => {
+    console.log(`amount: ${e.target.value}`);
+
+    setAmount(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  }
+
 
   return (
     <>
@@ -23,14 +45,17 @@ function App() {
       <h1>budget calculator</h1>
       <main className="App">
         <ExpenseForm />
-        <ExpenseList expenses={expenses}/>
+        <ExpenseList expenses={expenses} />
       </main>
-      <h1>total spending: { ' '} 
-          <span className="total">$ 
-          { expenses.reduce((acc, curr) => {
-              return (acc += curr.amount)
+      <h1>
+        total spending:{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return (acc += curr.amount);
           }, 0)}
-          </span></h1>
+        </span>
+      </h1>
     </>
   );
 }
