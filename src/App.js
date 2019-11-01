79aed0659fbfb40994f19df73021925c20a8a799
beata <beata@emc2wave.com>
2019-11-01 16:41:34 -0700

    clear functionality to setChange, setAmount

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ function App() {
     if(charge !== '' && amount > 0) {
       const singleExpense = {id:uuid(), charge, amount};
       setExpenses([...expenses, singleExpense]);
+      setCharge('');
+      setAmount('');
     } else {
       // handle alert
     }
