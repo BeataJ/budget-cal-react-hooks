2676cb0ac9581f36abe64528c36c5641ff686359
beata <beata@emc2wave.com>
2019-11-01 20:03:10 -0700

    add props handleDelete handleEdit to Item

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -7,7 +7,7 @@ const ExpenseList = ({expenses, handleDelete, handleEdit, clearItems}) => {
     <>
       <ul className="list">
         {expenses.map((expense) => {
-            return <Item key={expense.id} expense={expense} />
+            return <Item key={expense.id} expense={expense} handleDelete={handleDelete} handleEdit={handleEdit} />
         })}
       </ul>
         {expenses.length > 0 && <button className="btn" onClick={clearItems}>
